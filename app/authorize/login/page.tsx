1d4a8a23993a081bae403fc517393bdebe54b911
David Hayek <dhayek@charter.net>
2025-01-10 18:42:42 -0600

    added more implementations of the authentication layout.

--- app/authorize/login/page.tsx
+++ app/authorize/login/page.tsx
@@ -1,10 +1,100 @@
+"use client";
 import React from "react";
+import { Box, Button, Divider, Grid2, Paper, Stack, TextField } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Link from 'next/link'
 
+
+// TODO
+// 1.   add handlers for buttons
+// 2.   retrieve the tenant information if present, in order to show
+//      background colors and logos
+// 3.   if there is tenant information, then are the following enabled?
+//      a.  forgot password allowed
+//      b.  registration allowed
+//      c.  phone number as username allowed
+// 4.   Show the cancel button only when there is a _tk parameter, (which
+//      means that the user is coming from a known client and is doing
+//      the oauth2 protocol and so may want to cancel and go back to
+//      where they came from.
 const Login: React.FC = () => {
 
-    return <>
-        Hello from the login screen
-    </>
+    const theme = useTheme();
+    const isMd: boolean = useMediaQuery(theme.breakpoints.down("md"));
+    const isSm: boolean = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const maxWidth = isSm ? "90vw" : isMd ? "80vw" : "650px";
+    return (
+
+        <Paper
+            elevation={4}
+            sx={{ padding: 2, height: "100%", maxWidth: maxWidth }}
+        >
+            <Grid2 spacing={4} container size={{ xs: 12 }}>
+                <Grid2 size={{ xs: 12 }}>
+                    <div style={{marginBottom: "16px", fontWeight: "bold", fontSize: "1.2em"}}>Sign In</div>
+                    <TextField
+                        id="email"
+                        required={true}
+                        autoFocus={true}
+                        label="Email or phone number"
+                        name="email"
+                        fullWidth
+                    >
+                    </TextField>
+                </Grid2>
+                <Grid2 size={{ xs: 12 }}>
+                    <Stack
+                        direction={"row-reverse"}
+                    >
+                        <span><Link href="/" style={{color: "blue"}}>Forgot password?</Link></span>
+                    </Stack>
+                </Grid2>
+                <Grid2 size={{ xs: 12 }}>
+                    <Stack
+                        direction={"row-reverse"}
+                    >
+                        <Button
+                            disabled={false}
+                            variant="contained"
+                            sx={{ height: "100%", padding: "8px 32px 8px 32px", marginLeft: "8px" }}
+                        >Next</Button>
+
+                        <Button
+                            disabled={false}
+                            variant="contained"
+                            sx={{ height: "100%", padding: "8px 32px 8px 32px" }}
+                        >Cancel</Button>
+
+                    </Stack>
+                </Grid2>
+
+                <Grid2 size={{ xs: 12 }}
+                >
+                    <Divider></Divider>
+                </Grid2>
+                
+                <Grid2 size={{ xs: 12 }} textAlign={"center"}>
+                    <Stack
+                        direction={"row-reverse"}
+                        justifyItems={"center"}
+                        alignItems={"center"}
+                    >                        
+                        <Button
+                            disabled={false}
+                            variant="contained"
+                            sx={{ height: "100%", padding: "8px 32px 8px 32px", marginLeft: "8px" }}
+                        >Register</Button>
+
+                        <div style={{verticalAlign: "center"}}>Need to create an account?</div>
+                    </Stack>
+                </Grid2>
+
+            </Grid2>
+        </Paper>
+
+    )
 }
 
 export default Login;
--- components/layout/authentication-header.tsx
+++ components/layout/authentication-header.tsx
@@ -1,9 +1,22 @@
 "use client";
+import { Container } from "@mui/material";
 import React from "react";
 
 const AuthenticationHeader: React.FC = () => {
+
+
     return (
-        <div>Authentication header</div>
+        <div 
+            style={{backgroundColor: "#0313fc", width: "100%", height: "5vh", color: "white"}}
+
+        >
+            <Container
+                maxWidth="xl"
+            >
+                <div></div>
+            </Container>
+        </div>
+        
     )
 }
 
