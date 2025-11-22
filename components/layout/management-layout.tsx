8fcc50b710c6481a49a066afe3f42838e9890e81
David Hayek <dhayek@charter.net>
2025-11-22 12:27:23 -0600

    updating responsive design.

--- components/layout/management-layout.tsx
+++ components/layout/management-layout.tsx
@@ -48,7 +48,7 @@ const ManagementLayout: React.FC<Props> = ({children}) => {
                 <Box sx={{ flexGrow: 1,  }}>
                     <Grid2 size={12} container spacing={1} sx={{}}>                
                         <Grid2 
-                            size={{xs: 12, sm: 12, md: 3, lg: 2.4, xl: 2}} 
+                            size={{xs: 12, sm: 12, md: 3, lg: 2.2, xl: 2}} 
                             sx={{
                                 padding: breakPoints.isMedium ? "0px" : "8px",
                                 borderBottom: breakPoints.isMedium ? "solid 1px lightgrey" : "",
@@ -64,7 +64,7 @@ const ManagementLayout: React.FC<Props> = ({children}) => {
                         </Grid2>
                         
                         <Grid2  
-                            size={{xs: 12, sm: 12, md: 9, lg: 9.6, xl: 10}} 
+                            size={{xs: 12, sm: 12, md: 9, lg: 9.8, xl: 10}} 
                             sx={{padding: breakPoints.isMedium ? "8px" : "8px 8px 0px 8px", minHeight: breakPoints.isMedium ? "86vh" : "91vh"}}
                         >                            
                             <Grid2>{children}</Grid2>                            
