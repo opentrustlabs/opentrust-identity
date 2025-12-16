4fba04ad6b546e05860242975620622cb98d9831
David Hayek <dhayek@charter.net>
2025-12-15 21:04:28 -0600

    updating google kms impl and updating the env example file.

--- lib/kms/google-kms.ts
+++ lib/kms/google-kms.ts
@@ -3,6 +3,7 @@ import { KeyManagementServiceClient } from "@google-cloud/kms";
 import { MAX_ENCRYPTION_LENGTH } from "@/utils/consts";
 import { logWithDetails } from "../logging/logger";
 
+
 const {
     GOOGLE_KMS_PROJECT_ID,
     GOOGLE_KMS_LOCATION_ID,
@@ -14,7 +15,7 @@ const {
 const client = new KeyManagementServiceClient();
 const keyName = client.cryptoKeyPath(GOOGLE_KMS_PROJECT_ID || "", GOOGLE_KMS_LOCATION_ID || "", GOOGLE_KMS_KEY_RING_ID || "", GOOGLE_KMS_KEY_ID || "");
 const maxLength = MAX_PLAIN_TEXT_LENGTH ? parseInt(MAX_PLAIN_TEXT_LENGTH) : MAX_ENCRYPTION_LENGTH;
-
+const crc32c = require("fast-crc32c");
 class GoogleKMS extends Kms {
 
     public async encrypt(data: string, aad?: string): Promise<string | null> {
@@ -35,12 +36,19 @@ class GoogleKMS extends Kms {
         }
 
         try{
+            const plaintextCrc32c = crc32c.calculate(data);
             const [encryptResponse] = await client.encrypt({
                 name: keyName,
                 plaintext: data,
-                additionalAuthenticatedData: aad
+                additionalAuthenticatedData: aad,
+                plaintextCrc32c: {
+                    value: plaintextCrc32c
+                }
             });
 
+            if(!encryptResponse.verifiedPlaintextCrc32c){
+                logWithDetails("error", "Error encrypting with Google KMS: CRC checked failed.");
+            }
             if(encryptResponse.ciphertext){
                 return Buffer.from(encryptResponse.ciphertext);
             }
@@ -58,7 +66,7 @@ class GoogleKMS extends Kms {
     }
 
     public async decrypt(data: string, aad?: string): Promise<string | null> {
-        const decryptedData: Buffer | null = await this.decryptBuffer(Buffer.from(data, "base64"));
+        const decryptedData: Buffer | null = await this.decryptBuffer(Buffer.from(data, "base64"), aad);
         if(!decryptedData){
             return Promise.resolve(null);
         }
@@ -67,12 +75,21 @@ class GoogleKMS extends Kms {
 
     public async decryptBuffer(data: Buffer, aad?: string): Promise<Buffer | null> {
         try{
+            const ciphertextCrc32c = crc32c.calculate(data);
             const [decryptResponse ] = await client.decrypt({
                 name: keyName,
                 ciphertext: data,
-                additionalAuthenticatedData: aad
+                additionalAuthenticatedData: aad,
+                ciphertextCrc32c: {
+                    value: ciphertextCrc32c
+                }
             });
+            
             if(decryptResponse.plaintext){
+                if(crc32c.calculate(decryptResponse.plaintext) !== Number(decryptResponse.plaintextCrc32c?.value)){
+                    logWithDetails("error", "Error decrypting with Google KMS: CRC checked failed.");
+                    return null;
+                }
                 return Buffer.from(decryptResponse.plaintext);
             }
             else{
